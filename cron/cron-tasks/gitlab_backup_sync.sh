f383c4fa0866d541d257ee8ee8c4d7f323c0c843
OALH. <2267823623@qq.com>
2026-08-20 20:09:02 +0800

    Version: 4.0.1

--- cron/cron-tasks/gitlab_backup_sync.sh
+++ cron/cron-tasks/gitlab_backup_sync.sh
@@ -12,11 +12,18 @@ for backup in ${gitlab_backup_dir}/*; do
         echo -e "compare file:\n  ${backup}\n  ${dump}"
         
         if [ "$(basename ${backup})" == "$(basename ${dump})" ]; then
-            backup_md5sum=$(md5sum ${backup} | awk '{print $1}')
-            dump_md5sum=$(md5sum ${dump} | awk '{print $1}')
-            echo "md5sum ${backup}: $backup_md5sum"
-            echo "md5sum ${dump}: $dump_md5sum"
-            if [ "$backup_md5sum" == "$dump_md5sum" ]; then
+            #echo "file exists and same, md5sum check..."
+            #backup_md5sum=$(md5sum ${backup} | awk '{print $1}')
+            #dump_md5sum=$(md5sum ${dump} | awk '{print $1}')
+            #echo "md5sum ${backup}: $backup_md5sum"
+            #echo "md5sum ${dump}: $dump_md5sum"
+            #if [ "$backup_md5sum" == "$dump_md5sum" ]; then
+            echo "file exists and same, check file size..."
+            backup_size=$(stat -c %s ${backup})
+            dump_size=$(stat -c %s ${dump})
+            echo "file ${backup} size : $backup_size"
+            echo "file ${dump} size : $dump_size"
+            if [ "$backup_size" == "$dump_size" ]; then
                 echo "skip ${backup}"
                 hasDump=1
                 break
@@ -29,6 +36,10 @@ for backup in ${gitlab_backup_dir}/*; do
     if [ ${hasDump} -eq 0 ]; then
         echo ""
 
-        cp -v -p ${backup} ${gitlab_backup_dump_dir}/
+        #cp -v -p ${backup} ${gitlab_backup_dump_dir}/
+
+        # 单个tar文件复制，显示进度，断点续传
+        rsync -ahW --progress --partial ${backup} ${gitlab_backup_dump_dir}/
+
     fi
 done
