698a128169b1a184356f6e17ac160cd571eeded6
OALH. <2267823623@qq.com>
2026-08-20 20:31:38 +0800

    Version: 4.0.2

--- cron/cron-tasks/gitlab_backup_clear.sh
+++ cron/cron-tasks/gitlab_backup_clear.sh
@@ -7,7 +7,7 @@ for dir in $gitlab_backup_dir  $gitlab_backup_dump_dir; do
     echo "----------------------------------"
     echo "current dir is $dir"
     echo "check file create time:"
-    for file in $(find $dir -type f -mtime +5); do
+    for file in $(find $dir -type f -mtime +3); do
         echo "  rm $file"
         rm -f $file
     done
